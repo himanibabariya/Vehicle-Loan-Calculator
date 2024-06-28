a0d97646f8cb85a2c48026f4784aac11ec30d070
Himani Babariya <himanibabariya@gmail.com>
2024-06-27 23:50:22 -0400

    Home Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,29 @@ function App() {
     <>
       <h1 className='text-3xl text-center'>Vehicle Loan Calcuator</h1>
 
-      <div className=''>
-        <label htmlFor="">Vehicle Loan Amaount</label>
-        <input type="number" name="" id="" />
+      <div className='flex max-w-7xl justify-center mx-auto mt-10'>
+        <div className=''>
+          <label htmlFor="">Vehicle Loan Amaount</label>
+          <input type="number" name="" id="" />
 
-        <label htmlFor="">Down Payment</label>
-        <input type="number" name="" id="" />
+          <label htmlFor="">Down Payment</label>
+          <input type="number" name="" id="" />
 
-        <label htmlFor="">Interest rate</label>
-        <input type="number" name="" id="" />
+          <label htmlFor="">Interest rate</label>
+          <input type="number" name="" id="" />
 
-        <label htmlFor="">Pay it off on...</label>
-        <input type="number" name="" id="" />
-        <label htmlFor="">Months</label>
+          <label htmlFor="">Pay it off on...</label>
+          <input type="number" name="" id="" />
+          <label htmlFor="">Months</label>
 
-        <input type="range" name="" id="" />
+          <input type="range" name="" id="" />
+        </div>
+
+        <div>
+          <p>Based on what you entered, your vehicle loan monthly payment is</p>
+        </div>
+
       </div>
-
-      <div>
-      <p>Based on what you entered, your vehicle loan monthly payment is</p>
-      </div>
-
 
     </>
   )
