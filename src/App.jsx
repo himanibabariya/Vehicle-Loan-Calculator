cc0271c6f9d9f2f1dfb50e5752321ff752c0625c
Himani Babariya <himanibabariya@gmail.com>
2024-06-29 23:56:03 -0400

    Loan Calculation Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -8,6 +8,10 @@ function App() {
   const [interestRate, setinterestRate] = useState(0);
   const [payoffMonths, setPayoffMonths] = useState(0);
   const [monthlyPayment, setMonthlyPayment] = useState(0);
+
+  useEffect(()=>{
+    calculateMonthlyPayment();
+  }, [loanAmount, downPayment, interestRate, payoffMonths]);
 
   const calculateMonthlyPayment = () =>{
     const principal = loanAmount - downPayment;
@@ -33,36 +37,38 @@ function App() {
           <div className='my-6 relative'>
             <label htmlFor="loanAmount" className='w-72 inline-block text-xl '>Vehicle Loan Amaount</label>
             <i class="fa-solid fa-dollar-sign text-customRed absolute top-2 "></i>
-            <input placeholder='0' type="number" name="loanAmount" id="" className='h-9 border text-right border-gray-400 text-customRed hover:border-customRed focus:border-customRed' />
+            <input placeholder='0' type="number" name="loanAmount" id="" value={loanAmount} onChange={(e) => setLoanAmount(Number(e.target.value))} className='h-9 border text-right border-gray-400 text-customRed hover:border-customRed focus:border-customRed' />
           </div>
           <hr className=' border-gray-400' />
 
           <div className='my-6'>
             <label htmlFor="downPayment" className='w-72 inline-block text-xl '>Down Payment</label>
-            <input placeholder='0' type="number" name="downPayment" id="" className='h-9 border text-right border-gray-400 text-customRed ' />
+            <input placeholder='0' type="number" name="downPayment" id="" value={downPayment} onChange={(e) => setDownPayment(Number(e.target.value))} className='h-9 border text-right border-gray-400 text-customRed ' />
           </div>
           <hr className='border-gray-400'/>
 
           <div className='my-6'>
             <label htmlFor="interestRate" className='w-72 inline-block text-xl'>Interest rate</label>
-            <input placeholder='0.00' type="number" name="interestRate" id="" className='h-9 border text-right border-gray-400' />
+            <input placeholder='0.00' type="number" name="interestRate" id="" value={interestRate} onChange={(e) => setinterestRate(Number(e.target.value))} className='h-9 border text-right border-gray-400' />
           </div>
           <hr className='border-gray-400'/>
 
           <div className='my-6'>
             <label htmlFor="payoffMonths" className='w-72 inline-block text-xl'>Pay it off on...</label>
-            <input type="number" name="payoffMonths" id="" className='h-9 w-24 text-right<i class="fa-solid fa-dollar-sign"></i> border border-gray-400' />
+            <input type="number" name="payoffMonths" id="" value={payoffMonths}
+              onChange={(e) => setPayoffMonths(Number(e.target.value))} className='h-9 w-24 text-right<i class="fa-solid fa-dollar-sign"></i> border border-gray-400' />
             <label htmlFor="" className='ml-6'>Months</label>
           </div>
 
-          <input type="range" name="" id="" className='range-input' />
+          <input type="range" name="" id="" min="6" max='84' value={payoffMonths}
+            onChange={(e) => setPayoffMonths(Number(e.target.value))} className='range-input' />
         </div>
 
         <div className='w-px h-74 bg-gray-400'></div>
 
         <div>
           <p className='mx-20 mt-24 max-w-sm text-center '>Based on what you entered, your vehicle loan monthly payment is</p>
-          <p className='text-center text-5xl mt-6 text-customRed'>$50.00</p>
+          <p className='text-center text-5xl mt-6 text-customRed'>${monthlyPayment.toFixed(2)}</p>
         </div>
 
       </div>
